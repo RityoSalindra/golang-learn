18997868d11b33f945db96f661b60854f7a2b944
Rityo Salindra <nuruddin859@gmail.com>
2023-07-10 16:01:43 +0700

    refactor

---
+++ belajar-golang-level-akses/main.go
@@ -0,0 +1,17 @@
+package main
+
+// import (
+// 	. "belajar-golang-level-akses/library" // titik digunakan agar semua properti di package lain dapat dipanggil tanpa harus menuliskan nama packagenya
+// 	f "fmt"                                //import package alias agar lebih simple
+// )
+
+// func main() {
+// 	var s1 = Student{"Rityo", 21}
+// 	f.Println("name", s1.Name)
+// 	f.Println("grade", s1.Grade)
+
+// }
+
+func main() {
+	sayHello("Rityo")
+}
